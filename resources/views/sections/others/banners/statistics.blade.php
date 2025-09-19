1f1465e5fa8cb9b65e42631a76db05bc96daefe8
neavr <athayawildan@gmail.com>
2025-09-19 14:51:21 +0800

    change statistics banner

--- resources/views/sections/others/banners/statistics.blade.php
+++ resources/views/sections/others/banners/statistics.blade.php
@@ -1,7 +1,7 @@
 <div class="w-full">
     <div class="relative">
-        <img class="object-cover w-full h-40 sm:h-60 lg:h-80" src="{{ Storage::url("others/border.webp") }}" alt="border.webp">
-        <div class="absolute inset-0 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 items-center justify-center text-white bg-black/25 gap-4 px-6 h-[250px] sm:h-[350px] lg:h-80">
+        <img class="object-cover w-full h-64 sm:h-80 md:h-80 lg:h-80" src="{{ Storage::url("others/border.webp") }}" alt="border.webp">
+        <div class="absolute inset-0 grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-4 items-center justify-center text-white gap-4 px-6 h-[250px] sm:h-[350px] lg:h-80">
             <div class="flex flex-col items-center justify-center w-full text-center" data-aos="fade-up">
                 <h1 class="mb-2 text-3xl font-semibold break-words sm:text-4xl md:text-5xl lg:text-6xl">3+</h1>
                 <p class="text-base break-words sm:text-lg md:text-xl lg:text-2xl">Years Experience</p>
